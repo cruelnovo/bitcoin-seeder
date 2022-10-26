3097983f3b7e1a5fb6a452ea1bef5eed370e9ffd
Matthew Cummings <matt@gocummings.com>
2022-10-26 13:33:49 -0400

    rebase to vertcoin

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -6,7 +6,7 @@
 #include "serialize.h"
 #include "uint256.h"
 
-#define BITCOIN_SEED_NONCE  0x0539a019ca550825ULL
+#define BITCOIN_SEED_NONCE  0x0539a019ca550825
 
 using namespace std;
 
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/bitcoin-seeder:0.01/";
+    string ver = "/vertcoin-seeder:0.01/";
     uint8_t fRelayTxs = 0;
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight << fRelayTxs;
     EndMessage();
